d1bf6e1c90123ac4e00578a5a67371161b4bbcec
matiasc18 <matiascarulli@gmail.com>
2023-03-30 00:15:02 -0400

    Added layout wrapper

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import 'typeface-roboto'
 
+import Layout from '@/components/layout'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
