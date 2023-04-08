f51515b9f33efe26d2b8a8d8cc9eca20e92d8237
matiasc18 <matiascarulli@gmail.com>
2023-04-07 21:43:11 -0400

    Added query-building functions
    - Build query for editing an event and for editing a university profile

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,85 @@
+// Build SQL query for editing event details
+export function buildEditQuery(body, eventId) {
+  let query = 'UPDATE event SET';
+  let values = [];
+
+  // Only update what user provides in request body
+  if (body.category !== undefined) {
+    query += ' category = ?,';
+    values.push(body.category);
+  }
+  if (body.locationId !== undefined) {
+    query += ' location_id = ?,';
+    values.push(body.locationid);
+  }
+  if (body.contactEmail !== undefined) {
+    query += ' contact_email = ?,';
+    values.push(body.contactEmail);
+  }
+  if (body.contactName !== undefined) {
+    query += ' contact_name = ?,';
+    values.push(body.contactName);
+  }
+  if (body.contactPhone !== undefined) {
+    query += ' contact_phone = ?,';
+    values.push(body.contactPhone);
+  }
+  if (body.date !== undefined) {
+    query += ' date = ?,';
+    values.push(new Date(Date.parse(body.date)));
+  }
+  if (body.description !== undefined) {
+    query += ' description = ?,';
+    values.push(body.description);
+  }
+  if (body.timeEnd !== undefined) {
+    query += ' time_end = ?,';
+    values.push(new Date(Date.parse(body.timeEnd)));
+  }
+  if (body.timeStart !== undefined) {
+    query += ' time_start = ?,';
+    values.push(new Date(Date.parse(body.timeStart)));
+  }
+
+  // Remove trailing comma from query
+  query = query.slice(0, -1);
+  query += ' WHERE event_id = ?;';
+  values.push(eventId);
+
+  return { query, values };
+}
+
+// Build SQL query for editing university profile
+export function buildEditUniQuery(body, userId) {
+  let query = 'UPDATE university SET';
+  let values = [];
+
+  // Only update what user provides in request body
+  if (body.locationId !== undefined) {
+    query += ' location_id = ?,';
+    values.push(body.locationId);
+  }
+  if (body.name !== undefined) {
+    query += ' name = ?,';
+    values.push(body.name);
+  }
+  if (body.population !== undefined) {
+    query += ' population = ?,';
+    values.push(body.population);
+  }
+  if (body.description !== undefined) {
+    query += ' description = ?,';
+    values.push(body.description);
+  }
+
+  // Remove trailing comma from query
+  query = query.slice(0, -1);
+  query += ' WHERE superadmin_id = ?;';
+  values.push(userId);
+
+  return { query, values };
+}
+
 // Format Date object
 export function convertDateStringToLocal(data) {
   const newData = data.map((event) => {
