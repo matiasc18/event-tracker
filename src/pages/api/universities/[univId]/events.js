b5cd6590b2f96afa0715a6f0e518745514e75f29
matiasc18 <matiascarulli@gmail.com>
2023-04-07 21:46:17 -0400

    Created API endpoint: get all univ events
    - Uses verifyStudent middleware to determine if user is a student from that university

---
+++ src/pages/api/universities/[univId]/events.js
@@ -0,0 +1,31 @@
+import verifyStudent from '@/lib/api/middleware/verifyStudent';
+import connection from '@/lib/db'
+
+// /api/universities/:univId/events
+async function handler(req, res) {
+  // Get all events for a university
+  if (req.method === 'GET') {
+    const { univId } = req.query;
+
+    // Check if middleware caught an error
+    if (res.statusCode >= 400) {
+      return res;
+    }
+
+    try {
+      const query = "SELECT event.* FROM event INNER JOIN rso ON event.rso_id = rso.rso_id INNER JOIN user ON rso.admin_id = user.user_id INNER JOIN university ON user.univ_id = university.univ_id WHERE university.univ_id = ?;";
+      const values = [univId];
+      const [result] = await connection.execute(query, values);
+
+      if (result.length === 0) {
+        return res.status(404).json({ message: 'Events not found for this RSO' });
+      }
+
+      return res.status(200).json({ message: `All events for this RSO`, events: result });
+    } catch (error) {
+      return res.status(401).json({ message: 'Unauthorized', error: error?.response?.message || error?.message });
+    }
+  }
+}
+
+export default verifyStudent(handler);
