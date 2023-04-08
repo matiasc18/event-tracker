158f2237a45a9b0cab2d415f8dd3827f4ce7d7b1
matiasc18 <matiascarulli@gmail.com>
2023-04-07 21:49:24 -0400

    Updated mysql connection creation
    - Changed from createConnection to createPool

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -1,36 +1,32 @@
-import createConnection from '@/lib/db'
+import connection from '@/lib/db'
 import { sign } from 'jsonwebtoken'
 import { compare } from 'bcrypt'
 import Cookies from 'js-cookie'
 
-// Authenticate user login
+// /api/auth/login
 export default async function handler(req, res) {
+  // Authenticate user login
   if (req.method === 'POST') {
     const { email, password } = req.body;
 
-    // Email and password must be provided
+    // Email and password are not provided
     if (!email || !password)
       return res.status(400).json({ message: 'Email and password are required.' })
 
     try {
-      // Create connection and execute query
-      const connection = await createConnection();
-      const [rows] = await connection.execute('SELECT * FROM user WHERE email = ?', [email]);
+      const [result] = await connection.execute('SELECT user_id, password, role FROM user WHERE email = ?', [email]);
 
       // No user associated with provided email
-      if (rows.length === 0)
+      if (result.length === 0)
         return res.status(401).json({ message: 'Invalid email or password' });
-
-      const passwordMatch = await compare(password, rows[0].password);
-
+      
       // Password does not match user record
+      const passwordMatch = await compare(password, result[0].password);
       if (!passwordMatch)
         return res.status(401).json({ message: 'Invalid email or password' });
 
-      // Create and sign JWT token with user id
-      const token = sign({ user_id: rows[0].user_id }, process.env.JWT_SECRET);
-
-      // Set token as a cookie in the response header
+      // Create and sign JWT token with user id, set it as a cookie in response header
+      const token = sign({ user_id: result[0].user_id, role: result[0].role }, process.env.JWT_SECRET);
       Cookies.set('authToken', token, {
         maxAge: 60 * 60 * 24 * 7, // 1 week
         path: '/',
@@ -43,7 +39,7 @@ export default async function handler(req, res) {
         .setHeader('Set-Cookie', Cookies.get('authToken'))
         .json({ message: 'User logged in', authToken: token });
     } catch (error) {
-      return res.status(401).json({ message: 'Login failed', error: error });
+      return res.status(401).json({ message: 'Login failed', error: error?.response?.message || error?.message });
     }
   }
 }
