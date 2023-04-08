cd195143a7faac65e9dabe6ba30aa644abd3dbfa
matiasc18 <matiascarulli@gmail.com>
2023-04-07 21:50:52 -0400

    Created API endpoint: delete rso
    - Uses verifyAdmin middleware

---
+++ src/pages/api/admin/deleteRso/[rsoId].js
@@ -0,0 +1,27 @@
+import connection from '@/lib/db'
+import verifyAdmin from '@/lib/api/middleware/verifyAdmin'
+
+// /api/admin/deleteRso/:rsoId
+async function handler(req, res) {
+  // Delete rso
+  if (req.method === 'DELETE') {
+    const { rsoId } = req.query;
+
+    // Check if middleware caught an error
+    if (res.statusCode >= 400) {
+      return res;
+    }
+
+    try {
+      const query = 'DELETE FROM rso WHERE rso_id = ? AND admin_id = ?;';
+      const values = [rsoId, userId];
+      const result = await connection.execute(query, values);
+
+      return res.status(200).json({ message: 'RSO deleted' });
+    } catch (error) {
+      return res.status(400).json({ message: 'Error deleting RSO', error: error?.response?.message || error?.message });
+    }
+  }
+}
+
+export default verifyAdmin('rsoAdmin', handler);
