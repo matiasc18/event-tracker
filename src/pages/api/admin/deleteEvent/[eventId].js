a37c9413c22d686a3d4ba4431037abb032d36f8b
matiasc18 <matiascarulli@gmail.com>
2023-04-07 21:51:04 -0400

    Created API endpoint: delete event
    - Uses verifyAdmin middleware

---
+++ src/pages/api/admin/deleteEvent/[eventId].js
@@ -0,0 +1,26 @@
+import connection from '@/lib/db'
+import verifyAdmin from '@/lib/api/middleware/verifyAdmin'
+
+// /api/admin/deleteEvent/:eventId
+async function handler(req, res) {
+  // Delete event
+  if (req.method === 'DELETE') {
+    const { eventId } = req.query;
+
+    // Check if middleware caught an error
+    if (res.statusCode >= 400) {
+      return res;
+    }
+
+    try {
+      await connection.execute('DELETE FROM event WHERE event_id = ?;', [eventId]);
+
+      return res.status(200).json({ message: 'Event deleted' });
+    } catch (error) {
+      return res.status(400).json({ message: 'Error deleting event', error: error?.response?.message || error?.message });
+    }
+  }
+}
+
+// Middleware checks if the user is the rso's admin
+export default verifyAdmin('eventAdmin', handler);
