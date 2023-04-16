5f7899128df7ed8a886d074407a63d6b8230ceab
matiasc18 <matiascarulli@gmail.com>
2023-04-15 22:56:16 -0400

    Update index.js

--- src/pages/home/components/profile-preview/index.js
+++ src/pages/home/components/profile-preview/index.js
@@ -1,25 +1,27 @@
-import styles from './profile_preview.module.css'
+import styles from './styles/profile-preview.module.scss'
 import { RiAccountCircleFill } from 'react-icons/ri'
 
-export default function ProfilePreview() {
+export default function ProfilePreview({ user }) {
 
   return (
-    <section className={styles['profile-snippet']}>
-      <h1>Profile</h1>
-      <hr className="main-hr" />
-      <div className={styles['profile-details']}>
-        <RiAccountCircleFill className={styles['profile-icon']} />
-        <span className={styles['profile-name']}>Matias Carulli</span>
-        <span className={styles['profile-uni']}>University of Central Florida</span>
-        <div className={styles['rso-container']}>
-          <span className={styles['profile-rso']}>RSO memberships</span>
-          <span className={styles['profile-rso-count']}>4</span>
+    <>
+      {user && <section className={styles['profile-snippet']}>
+        <h1>Profile</h1>
+        <hr className="main-hr" />
+        <div className={styles['profile-details']}>
+          <RiAccountCircleFill className={styles['profile-icon']} />
+          <span className={styles['profile-name']}>{user.first_name} {user.last_name}</span>
+          <span className={styles['profile-uni']}>{user.univ_name}</span>
+          <div className={styles['rso-container']}>
+            <span className={styles['profile-rso']}>RSO memberships</span>
+            <span className={styles['profile-rso-count']}>{user.memberships}</span>
+          </div>
+          <div className={styles['rso-container']}>
+            <span className={styles['profile-rso']}>RSOs owned</span>
+            <span className={styles['profile-rso-count']}>{user.ownerships}</span>
+          </div>
         </div>
-        <div className={styles['rso-container']}>
-          <span className={styles['profile-rso']}>RSOs owned</span>
-          <span className={styles['profile-rso-count']}>1</span>
-        </div>
-      </div>
-    </section>
+      </section>}
+    </>
   )
 }
