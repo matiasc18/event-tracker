8f803cfa6f0a7ead103c5536f5db0caf7659d4ef
matiasc18 <matiascarulli@gmail.com>
2023-04-15 22:52:08 -0400

    API for getting all public events

--- src/pages/api/events/public.js
+++ src/pages/api/events/public.js
@@ -5,11 +5,13 @@ export default async function handler(req, res) {
   // Get all public events
   if (req.method === 'GET') {
     try {
-      const [result] = await connection.execute('SELECT * FROM event E WHERE E.type = "public";');
+      const query = 'SELECT E.*, R.name AS rso_name, L.name AS location_name, U.abbreviation FROM event E INNER JOIN rso R ON R.rso_id = E.rso_id INNER JOIN location L ON L.location_id = E.location_id INNER JOIN university U ON U.univ_id = E.univ_id WHERE E.type = "public";';
+      
+      const [result] = await connection.execute(query);
 
       return res.status(200).json({ events: result });
     } catch (error) {
-      return res.status(401).json({ message: 'Unauthorized', error: error?.response?.message || error?.message });
+      return res.status(401).json({ message: 'Error getting events', error: error?.response?.message || error?.message });
     }
   }
 }
