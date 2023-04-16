d5f1ed0bce4828b3e0db0c7a220e24525aacfa48
matiasc18 <matiascarulli@gmail.com>
2023-04-15 22:56:36 -0400

    Updated to get live events from DB

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,73 +1,80 @@
-import EventsSideBar from './components/events/eventSideBar'
-import EventsControl from './components/events/eventControl'
-import ProfilePreview from './components/profile-preview';
-import { convertDateStringToLocal } from '@/utils/utils';
-import Events from './components/events/events';
-import styles from './styles/home.module.css'
-import NavBar from '@/components/layout/components/navBar'
+import EventControl from '@/components/events/eventControl'
+import EventSideBar from '@/components/events/eventSideBar'
+import { convertDateStringToLocal } from '@/utils/utils'
+import Events from '../../components/events/events'
 import { useEffect, useState } from 'react'
-import data from '@/data/today.json'
 import Head from 'next/head'
-import Layout from '@/components/layout';
+import { getAllEvents } from '@/lib/api/client/events'
+
+// Remove dash and capitalize title
+const modifyTitle = (selected) => {
+  let newTitle = selected;
+  // Replace dashes (-) with a space ( )
+  newTitle = newTitle.replace(/-/g, " ");
+
+  // Capitalize first letter
+  if (selected == 'this-week' || selected == 'this-month') {
+    newTitle = newTitle.charAt(0).toUpperCase() + newTitle.slice(1);
+  }
+  // Capitalize first letter per word
+  else {
+    newTitle = newTitle.replace(/(^|\s)\w/g, (match) => {
+      return match.toUpperCase();
+    })
+  }
+
+  return newTitle;
+};
 
 export default function Home({ events }) {
   // Event list state
   const [selected, setSelected] = useState('this-week');
   const [title, setTitle] = useState('This week');
 
-  // Convert selected into title
+  // Convert id to proper title
   useEffect(() => {
-    const modifyTitle = (newTitle) => {
-      // Replace dashes (-) with a space ( )
-      newTitle = newTitle.replace(/-/g, " ");
-
-      // Capitalize first letter
-      if (selected == 'this-week' || selected == 'this-month') {
-        newTitle = newTitle.charAt(0).toUpperCase() + newTitle.slice(1);
-      }
-      // Capitalize first letter per word
-      else {
-        newTitle = newTitle.replace(/(^|\s)\w/g, (match) => {
-          return match.toUpperCase();
-        })
-      }
-
-      return newTitle;
-    };
-
     setTitle(modifyTitle(selected));
   }, [selected]);
 
   return (
-    <Layout>
+    <>
       <Head>
         <title>Event tracker</title>
       </Head>
-      {/* <NavBar /> */}
-      <main className="main">
-        <div className={styles['home-container']}>
-          <EventsSideBar selected={selected} setSelected={setSelected} />
-          <div className={styles['events-container']}>
-            <EventsControl title={title} />
+      <main className="main" id="top">
+        <div className="home-container">
+          <EventSideBar selected={selected} setSelected={setSelected} />
+          <div className="events-container">
+            <EventControl title={title} />
             <hr className="main-hr" />
             <Events events={events} />
           </div>
-          <ProfilePreview />
         </div>
       </main>
-    </Layout>
+    </>
   )
 }
 
-//TODO timezone error when converting date to Fri, Apr 13 2023 8:00 AM
-//TODO .split not working bc data isnt existent when convertDateStringToLocal(events) is called
-/* Return list of events with their descriptions */
-export async function getStaticProps() {
-  const events = data;
+// Get all events viewable to the user
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const authToken = req.headers.cookie?.split('; ').find(row => row.startsWith('authToken='))?.split('=')[1];
+  const response = await getAllEvents(authToken);
+
+  // Sort most recent
+  response.sort((a, b) => new Date(a.date) - new Date(b.date));
+  // Capitalize the 'type' of each event
+  const events = response.map((event) => {
+    return {
+      ...event,
+      type: event.type === 'rso' ? event.type.toUpperCase() : event.type.charAt(0).toUpperCase() + event.type.slice(1)
+    };
+  });
+  convertDateStringToLocal(events);
 
   return {
     props: {
-      events: convertDateStringToLocal(events)
-    },
+      events: events
+    }
   }
 }
