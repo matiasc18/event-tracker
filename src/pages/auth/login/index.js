a98f191bda68c458d5f11bca644a9bc36aa594d6
matiasc18 <matiascarulli@gmail.com>
2023-04-15 22:55:14 -0400

    Removed Google and Discord login

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import { useAuth } from '@/contexts/AuthContext'
-import styles from '../styles/auth.module.css'
+import styles from '../styles/auth.module.scss'
 import { FcGoogle } from 'react-icons/fc'
 import { BsGithub } from 'react-icons/bs'
 import Layout from '@/components/layout'
@@ -14,8 +14,8 @@ export default function Login({ loggedIn }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  
-  const { accessLoginStatus } = useAuth();
+
+  const { updateLoginStatus, loginStatus } = useAuth();
   const router = useRouter();
   const userRef = useRef(); // For setting focus on first input field
 
@@ -25,7 +25,7 @@ export default function Login({ loggedIn }) {
     userRef.current.focus();
 
     // Get latest login status
-    if (accessLoginStatus('update', null))
+    if (loginStatus)
       router.push('/home');
   }, []);
 
@@ -46,7 +46,7 @@ export default function Login({ loggedIn }) {
       setErrorMessage(response?.error?.response?.data?.message);
     else {
       // Set login status to true go to home page
-      accessLoginStatus('set', true);
+      updateLoginStatus(true);
       router.push('/home');
     }
   };
@@ -56,47 +56,49 @@ export default function Login({ loggedIn }) {
       <Head>
         <title>Log in to event tracker</title>
       </Head>
-      <main className={`${styles['auth-page']} main`}>
+      <main className={`${styles['auth-page']} main`} id="top">
         <form id="login-form" className={styles['auth-form']} autoComplete="off" onSubmit={handleSubmit}>
-          <div className={styles['auth-title']}>
-            <h1>Sign in</h1>
-          </div>
-          <div className={styles['input-group']}>
-            <input
-              type="text"
-              id="email"
-              ref={userRef}
-              className={styles['auth-input']}
-              placeholder="Email"
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="email" className={styles['auth-label']}>Email</label>
-          </div>
-          <div className={styles['input-group']}>
-            <input
-              type="password"
-              id="password"
-              className={styles['auth-input']}
-              placeholder="Password"
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <label htmlFor="password" className={styles['auth-label']}>Password</label>
-          </div>
-          {/* If theres an error message, display it */}
-          {errorMessage && <span>{errorMessage}</span>}
-          <div className={styles['submit-container']}>
-            <button className={styles['auth-submit']} form="login-form" disabled={(email && password) ? false : true}>Sign in</button>
-          </div>
-          <div className={styles['auth-footer']}>
-            <span>Or</span>
-            <div className={styles['submit-container']}>
-              <FcGoogle className={styles['next-auth']} onClick={() => signIn("google")} />
-              <BsGithub className={styles['next-auth']} />
+          <section className={styles['login-container']}>
+            <div className={styles['auth-title']}>
+              <h1>Sign in</h1>
             </div>
-            <span>Don't have an account? <Link href="/auth/register">Sign Up</Link></span>
-          </div>
+            <div className={styles['input-group']}>
+              <input
+                type="text"
+                id="email"
+                ref={userRef}
+                className={styles['auth-input']}
+                placeholder="Email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label htmlFor="email" className={styles['auth-label']}>Email</label>
+            </div>
+            <div className={styles['input-group']}>
+              <input
+                type="password"
+                id="password"
+                className={styles['auth-input']}
+                placeholder="Password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label htmlFor="password" className={styles['auth-label']}>Password</label>
+            </div>
+            {/* If theres an error message, display it */}
+            {errorMessage && <span>{errorMessage}</span>}
+            <div className={styles['submit-container']}>
+              <button className={styles['auth-submit']} form="login-form" disabled={(email && password) ? false : true}>Sign in</button>
+            </div>
+            <div className={styles['auth-footer']}>
+              {/* <span>Or</span>
+              <div className={styles['submit-container']}>
+                <FcGoogle className={styles['next-auth']} onClick={() => signIn("google")} />
+                <BsGithub className={styles['next-auth']} />
+              </div> */}
+              <span>{`Don't have an account?`} <Link href="/auth/register">Sign Up</Link></span>
+            </div>
+          </section>
         </form>
       </main>
     </Layout>
